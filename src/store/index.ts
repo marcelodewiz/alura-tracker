578dd0e0e139e3f8cca4c726e9b188f10028ba05
Marcelo Migotto <marcelo.dewiz@gmail.com>
2023-06-08 23:39:16 -0300

    Aula 3 Evoluindo o Alura Tracker

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,47 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import { InjectionKey } from "vue";
+import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ALTERA_TAREFA, EXCLUIR_PROJETO, EXCLUIR_TAREFA } from "./tipo-mutacoes";
+import ITarefa from "@/interfaces/ITarefa";
 
 interface Estado {
-    projetos: IProjeto[]
+    projetos: IProjeto[],
+    tarefas: ITarefa[]
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        projetos: []
+        projetos: [],
+        tarefas: []
     },
     mutations: {
-        'ADICIONA_PROJETO'(state, nomeDoProjeto: string) {
+        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
             const projeto = {
                 id: new Date().toISOString(),
                 nome: nomeDoProjeto
             } as IProjeto
             state.projetos.push(projeto)
-        }
+        },
+        [ALTERA_PROJETO](state, projeto: IProjeto) {
+            const index = state.projetos.findIndex(proj => proj.id == projeto.id)
+            state.projetos[index] = projeto
+        },
+        [EXCLUIR_PROJETO](state, id: string) {
+            state.projetos = state.projetos.filter(projeto => projeto.id != id)
+        },
+        [ADICIONA_TAREFA](state, tarefa: ITarefa) {
+            tarefa.id = new Date().toISOString(),
+                state.tarefas.push(tarefa)
+        },
+        [ALTERA_TAREFA](state, tarefa: ITarefa) {
+            const index = state.tarefas.findIndex(tar => tar.id == tarefa.id)
+            state.tarefas[index] = tarefa
+        },
+        [EXCLUIR_TAREFA](state, id: string) {
+            state.tarefas = state.tarefas.filter(tarefa => tarefa.id != id)
+        },
     }
 })
 
